dd5b48e100b354b38554119c822830f5617fba78
MIJanda <mokiliisaacjanda@gmail.com>
2020-12-31 18:36:52 +0300

    created auth routes, an auth controller, and auth views

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
@@ -12,9 +13,11 @@ app.set('view engine', 'ejs');
 // database connection
 const dbURI = 'mongodb://localhost/node-auth';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+  .then((result) => app.listen(5000))
   .catch((err) => console.log(err));
 
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+// auth routes
+app.use(authRoutes);
